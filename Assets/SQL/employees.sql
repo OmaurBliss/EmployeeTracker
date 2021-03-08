34bb1d043152fb6530d082054b4a9df96d442580
Omaur B <omaurbliss@gmail.com>
2021-03-08 09:08:21 -0600

    changed sql

--- Assets/SQL/employees.sql
+++ Assets/SQL/employees.sql
@@ -28,29 +28,29 @@ use employees;
 INSERT INTO department
     (name)
 VALUES
-    ('Operations'),
+    ('Production'),
     ('Analystics'),
     ('Marketing'),
     ('Executive');
 INSERT INTO role
     (title, salary, department_id)
 VALUES
-    ('General Manager', 11000000, 1),
-    ('Coach', 4000000, 1),
-    ('Team Lead Analyst', 15000000, 2),
-    ('Team Analyst', 8000000, 2),
+    ('Production Lead', 5000000, 1),
+    ('Audio Tech 1', 4000000, 1),
+    ('Video Tech 1', 40000000, 2),
+    ('Marketing Manager', 8000000, 2),
     ('Media Manager', 7000000, 3),
     ('Media Specialist', 3000000, 3),
     ('CEO', 45000000, 4),
-    ('CEO Assistant', 25000000, 4);
+    ('VP', 25000000, 4);
 INSERT INTO employee
     (first_name, last_name, role_id, manager_id)
 VALUES
-    ('John', 'Stockton', 1, NULL),
-    ('Karl', 'Malone', 2, 1),
-    ('Michael', 'Jordan', 3, NULL),
-    ('Steve', 'Kerr', 4, 3),
-    ('Shawn', 'Kemp', 5, NULL),
-    ('Gary', 'Peyton', 6, 5),
-    ('Magic', 'Johnson', 7, NULL),
-    ('Larry', 'Bird', 8, 7);
+    ('Amanda', 'Hugandkiss', 1, NULL),
+    ('Ilean', 'Sideways', 2, 1),
+    ('Makeya', 'Wannascream', 3, NULL),
+    ('Dan', 'Delion', 4, 3),
+    ('Shawn', 'OftheDead', 5, NULL),
+    ('Pat', 'Nleather', 6, 5),
+    ('Mongo', 'Smash', 7, NULL),
+    ('Serine', 'Views', 8, 7);
